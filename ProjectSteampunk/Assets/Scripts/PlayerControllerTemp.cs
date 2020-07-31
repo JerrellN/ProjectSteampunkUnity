17021c27c917681c1292bb2ab67e290bf3f865da
JerrellN <nolen.jerrell@hotmail.com>
2020-07-30 21:20:58 -0500

    Basic animations seperated, made movement script

---
+++ ProjectSteampunk/Assets/Scripts/PlayerControllerTemp.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerTemp : MonoBehaviour
+{
+    [Header("Base Setting")]
+    public int playerId;
+
+    [Space]
+    [Header("Movement Attributes")]
+    public Vector2 movementDirection;
+    public float movementSpeed;
+
+    [Space]
+    [Header("References")]
+    public Rigidbody2D rb;
+
+    [Space]
+    [Header("Character Attributes")]
+    public float movementBaseSpeed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+        Move();
+    }
+
+    void ProcessInput()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        movementSpeed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
+        movementDirection.Normalize();
+    }
+
+    void Move()
+    {
+        rb.velocity = movementDirection * movementSpeed * movementBaseSpeed;
+    }
+}
+
