25c195e7dab276b329b20534d90a466b46ea2db7
Gilmoremikal122 <Gilmoremikal122@gmail.com>
2020-08-06 16:04:42 -0500

    Sliced and movement
    Sliced 99% of sprites and worked on movement

---
+++ ProjectSteampunk/Assets/Scripts/PlayerControllerTemp.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerTemp : MonoBehaviour
+{
+    [Header("Base Setting")]
+    public int playerId;
+
+    [Space]
+    [Header("Movement Attributes")]
+    public Vector2 movementDirection;
+    public float Speed;
+
+    [Space]
+    [Header("References")]
+    public Rigidbody2D rb;
+
+    [Space]
+    [Header("Character Attributes")]
+    public float movementBaseSpeed = 1.0f;
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+        Move();
+        Animate();
+    }
+
+    void ProcessInput()
+    {
+        movementDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Speed = Mathf.Clamp(movementDirection.magnitude, 0.0f, 1.0f);
+        movementDirection.Normalize();
+    }
+
+    void Move()
+    {
+        rb.velocity = movementDirection * Speed * movementBaseSpeed;
+
+    }
+
+    void Animate()
+    {
+        animator.SetFloat("Horizontal", movementDirection.x);
+        animator.SetFloat("Vertical", movementDirection.y);
+		animator.SetFloat("Speed", Speed);
+    }
+}
+
