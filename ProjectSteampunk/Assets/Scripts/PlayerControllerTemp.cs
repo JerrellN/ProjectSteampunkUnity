d3dee6c68b45a875c6651119387a619356dc3e42
JerrellN <nolen.jerrell@hotmail.com>
2020-08-13 18:47:40 -0500

    Updated walk with 8 directions, making it fluent is next.

--- ProjectSteampunk/Assets/Scripts/PlayerControllerTemp.cs
+++ ProjectSteampunk/Assets/Scripts/PlayerControllerTemp.cs
@@ -48,9 +48,12 @@ public class PlayerControllerTemp : MonoBehaviour
     }
 
     void Animate()
-    {
-        animator.SetFloat("Horizontal", movementDirection.x);
-        animator.SetFloat("Vertical", movementDirection.y);
+    {   
+        if(movementDirection != Vector2.zero)
+        {
+            animator.SetFloat("Horizontal", movementDirection.x);
+            animator.SetFloat("Vertical", movementDirection.y);
+        }
         animator.SetFloat("Speed", movementSpeed);
     }
 }
